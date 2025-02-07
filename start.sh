b30b43842a0d0f54fe2d21d5f6bfb4225055a6e8
Ruben de Roos <deroosruben@gmail.com>
2025-02-07 22:08:21 +0100

    chrown docker

--- start.sh
+++ start.sh
@@ -5,6 +5,11 @@ if [ -z "$REPO" ] || [ -z "$ACCESS_TOKEN" ]; then
     exit 1
 fi
 
+# Fix Docker socket permissions
+if [ -e /var/run/docker.sock ]; then
+    sudo chmod 666 /var/run/docker.sock
+fi
+
 echo "Configuring GitHub Actions Runner..."
 
 ./config.sh --url $REPO --token $ACCESS_TOKEN
